6b6bf3d835cc7fa26df57039fa1292d1303fd071
jazuflo <jazuflo@gmail.com>
2016-12-11 18:14:13 -0500

    Software Evolucionado
    Software evolucionado con la adición del requerimiento para mostrar el mensaje de condición de riesgo.

---
+++ SoftwareIMC/SoftwareIMC/frmIMC.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SoftwareIMC
+{
+    partial class frmIMC
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.nudAltura = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudPeso = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nudIMC = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCondición = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAltura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIMC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.nudAltura);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nudPeso);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Datos";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(100, 88);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // nudAltura
+            // 
+            this.nudAltura.DecimalPlaces = 2;
+            this.nudAltura.Location = new System.Drawing.Point(118, 50);
+            this.nudAltura.Name = "nudAltura";
+            this.nudAltura.Size = new System.Drawing.Size(120, 20);
+            this.nudAltura.TabIndex = 1;
+            this.nudAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ingrese Altura (Mt)";
+            // 
+            // nudPeso
+            // 
+            this.nudPeso.DecimalPlaces = 2;
+            this.nudPeso.Location = new System.Drawing.Point(118, 24);
+            this.nudPeso.Name = "nudPeso";
+            this.nudPeso.Size = new System.Drawing.Size(120, 20);
+            this.nudPeso.TabIndex = 0;
+            this.nudPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Peso (Kg)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCondición);
+            this.groupBox2.Controls.Add(this.nudIMC);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 73);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado";
+            // 
+            // nudIMC
+            // 
+            this.nudIMC.DecimalPlaces = 2;
+            this.nudIMC.Enabled = false;
+            this.nudIMC.Location = new System.Drawing.Point(118, 17);
+            this.nudIMC.Name = "nudIMC";
+            this.nudIMC.ReadOnly = true;
+            this.nudIMC.Size = new System.Drawing.Size(120, 20);
+            this.nudIMC.TabIndex = 0;
+            this.nudIMC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado de IMC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Condición de Riesgo";
+            // 
+            // txtCondición
+            // 
+            this.txtCondición.Enabled = false;
+            this.txtCondición.Location = new System.Drawing.Point(118, 42);
+            this.txtCondición.Name = "txtCondición";
+            this.txtCondición.Size = new System.Drawing.Size(120, 20);
+            this.txtCondición.TabIndex = 3;
+            // 
+            // frmIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 234);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmIMC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de IMC";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAltura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIMC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.NumericUpDown nudAltura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudPeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown nudIMC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCondición;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SoftwareIMC/SoftwareIMC/frmIMC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SoftwareIMC
+{
+    public partial class frmIMC : Form
+    {
+        public frmIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            nudIMC.Value = nudPeso.Value / (nudAltura.Value * nudAltura.Value);
+            if (nudIMC.Value >= 40)
+                txtCondición.Text = "Muy Severo";
+            else if (nudIMC.Value >= 35)
+                txtCondición.Text = "Severo";
+            else if (nudIMC.Value >= 30)
+                txtCondición.Text = "Moderado";
+            else if (nudIMC.Value >= 25)
+                txtCondición.Text = "Aumentado";
+            else if (nudIMC.Value >= (decimal)18.5)
+                txtCondición.Text = "Promedio";
+        }
+    }
+}
